e82967574adfa12055cfec3fc0d5eb72bb5b265e
Adrienne Lim <adriennelim.mc@gmail.com>
2019-04-02 15:59:06 -0700

    complete basic functionality for review summary statssuch as average and total rating

--- client/src/components/ReviewsSummary.jsx
+++ client/src/components/ReviewsSummary.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const ReviewsSummary = (props) => {
+const ReviewsSummary = ({ reviews }) => {
+    let averageRating = 0;
+    if (reviews.length) {
+        averageRating = getAverageRating(reviews)
+    }
     return (
         <div className="container-summary">
             <hr />
             <h2 className="title-summary">Reviews Summary</h2>
             <div className="container-stats-chart">
                 <div className="summary-stats">
-                    <div>Summary stats</div>
+                    <div className="average-rating">{averageRating}</div>
+                    <div className="stars">Stars: {Math.round(averageRating)}</div>
+                    <div className="other-reviews" href="#">{`${reviews.length} reviews`}</div>
                 </div>
                 <div className="summary-chart">
                     <div>Summary chart</div>
@@ -15,6 +21,14 @@ const ReviewsSummary = (props) => {
             </div>
         </div>
     )
+};
+
+const getAverageRating = (reviews) => {
+    let sumOfRatings = 0;
+    sumOfRatings = reviews.reduce((sum, review) => {
+        return sum + review.rating;
+    }, 0)
+    return Number( (sumOfRatings / reviews.length).toFixed(1) );
 };
 
 export default ReviewsSummary;
