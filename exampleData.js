7cec896d03e93db73e6165ff23b8b3b0c98079e0
Adrienne Lim <adriennelim.mc@gmail.com>
2019-04-02 08:24:22 -0700

    added some sample data for test purposes

---
+++ exampleData.js
@@ -0,0 +1,145 @@
+module.exports = [
+    {
+        "id": 36,
+        "rating": 4,
+        "title": "necessitatibus eum sed",
+        "text_body": "Odit quo ducimus velit quas sit earum. Distinctio a alias laboriosam harum. Sequi nam numquam consequatur dolor quis.",
+        "created_at": "2017-10-04T07:00:00.000Z",
+        "store_name": "Kris Inc",
+        "helpful_score": 356,
+        "name": "Curt O'Conner",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/collegeman/128.jpg"
+    },
+    {
+        "id": 180,
+        "rating": 2,
+        "title": "nobis quidem in",
+        "text_body": "Impedit qui omnis modi optio fuga. Exercitationem dolorem quod error eos assumenda et. Doloribus vitae ratione assumenda placeat et non in.",
+        "created_at": "2015-07-24T07:00:00.000Z",
+        "store_name": "Feest - McDermott",
+        "helpful_score": 10,
+        "name": "Miss Katrine Kihn",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/davidbaldie/128.jpg"
+    },
+    {
+        "id": 182,
+        "rating": 1,
+        "title": "voluptate dolore nam",
+        "text_body": "Omnis fuga perferendis voluptate in quis facere numquam totam voluptatem. Inventore et aut eveniet nam provident nam et sed reprehenderit. Qui laboriosam at non omnis. Et ducimus quis recusandae vel. Amet et suscipit.",
+        "created_at": "2012-10-05T07:00:00.000Z",
+        "store_name": "Feest, Barton and Smitham",
+        "helpful_score": 460,
+        "name": "Kaela Kuphal",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/shadeed9/128.jpg"
+    },
+    {
+        "id": 318,
+        "rating": 1,
+        "title": "et quos nesciunt",
+        "text_body": "Quia nam animi aut. Sed omnis nesciunt provident sint quo eveniet voluptatum cum. Commodi et quibusdam assumenda sed sequi quis aut ut. Dicta non rem minima.",
+        "created_at": "2013-02-21T08:00:00.000Z",
+        "store_name": "Spencer - Williamson",
+        "helpful_score": 141,
+        "name": "Blaze Herzog",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/stefvdham/128.jpg"
+    },
+    {
+        "id": 347,
+        "rating": 3,
+        "title": "facere error facere",
+        "text_body": "Animi dolorem dolores aliquid ullam distinctio. Et numquam animi sed et dolores doloremque. Recusandae esse sapiente est. Et nihil deleniti officiis ratione sed ut eum doloribus. Voluptas quos accusantium dignissimos ut hic.",
+        "created_at": "2011-11-03T07:00:00.000Z",
+        "store_name": "Kilback, Schaden and Stamm",
+        "helpful_score": 355,
+        "name": "Kieran DuBuque",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/carlosblanco_eu/128.jpg"
+    },
+    {
+        "id": 574,
+        "rating": 5,
+        "title": "ut tenetur quibusdam",
+        "text_body": "Consequatur exercitationem nihil. Amet est iste qui. Et ut aliquam voluptas ex ut. Consectetur veniam nihil eos. Nam doloribus veritatis animi quibusdam.",
+        "created_at": "2014-06-24T07:00:00.000Z",
+        "store_name": "Fahey - Wiza",
+        "helpful_score": 427,
+        "name": "Dr. Paolo Lowe",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/switmer777/128.jpg"
+    },
+    {
+        "id": 582,
+        "rating": 2,
+        "title": "velit voluptatem beatae",
+        "text_body": "Provident voluptatem reprehenderit eius ut vel culpa illum est est. Minus consectetur amet provident amet et quia possimus nemo enim. Cumque non ea. Officiis excepturi unde quibusdam optio cupiditate. Qui suscipit consequatur voluptate deleniti.",
+        "created_at": "2016-06-16T07:00:00.000Z",
+        "store_name": "Weissnat - Wintheiser",
+        "helpful_score": 171,
+        "name": "Abigale Wilkinson",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/artvavs/128.jpg"
+    },
+    {
+        "id": 697,
+        "rating": 4,
+        "title": "minus incidunt ad",
+        "text_body": "Rerum nisi omnis et enim ullam a impedit eos. Exercitationem incidunt a quisquam sed. Eligendi ullam et consequuntur quidem reiciendis iste consectetur est inventore.",
+        "created_at": "2016-09-05T07:00:00.000Z",
+        "store_name": "West - Schmidt",
+        "helpful_score": 56,
+        "name": "Lucious Krajcik",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/gcmorley/128.jpg"
+    },
+    {
+        "id": 715,
+        "rating": 2,
+        "title": "consequuntur aut repudiandae",
+        "text_body": "Eum alias recusandae suscipit. Itaque non et consectetur sunt nisi provident omnis quia. Repudiandae optio voluptas facere et quas et.",
+        "created_at": "2011-11-21T08:00:00.000Z",
+        "store_name": "Schamberger, Dietrich and Hansen",
+        "helpful_score": 270,
+        "name": "Curt O'Conner",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/collegeman/128.jpg"
+    },
+    {
+        "id": 763,
+        "rating": 4,
+        "title": "dolorum hic quam",
+        "text_body": "Quasi porro in voluptatum consectetur. Sapiente iste rerum illum. Quia corporis porro tempore.",
+        "created_at": "2013-02-08T08:00:00.000Z",
+        "store_name": "Rempel and Sons",
+        "helpful_score": 231,
+        "name": "Reba Kulas",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/webtanya/128.jpg"
+    },
+    {
+        "id": 779,
+        "rating": 1,
+        "title": "ut ut vero",
+        "text_body": "Iusto harum voluptas animi. Fugiat amet reiciendis et quae. Veritatis nihil cupiditate harum accusamus et consectetur maxime amet debitis. Natus placeat aliquid est. Sint quis qui provident totam laudantium sapiente non.",
+        "created_at": "2010-01-12T08:00:00.000Z",
+        "store_name": "Dicki - Lynch",
+        "helpful_score": 313,
+        "name": "Malika Turcotte II",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/arthurholcombe1/128.jpg"
+    },
+    {
+        "id": 812,
+        "rating": 2,
+        "title": "et amet ut",
+        "text_body": "Aperiam officia ea atque a incidunt optio nisi sunt. Nihil est et voluptas reprehenderit eos saepe molestiae. Animi minus voluptatibus aliquid aperiam et sed quas minima qui. Esse aut quos rem. Et ut esse quas natus tempora facilis et rerum illo. Consectetur quia sed dolor similique non repudiandae est doloribus.",
+        "created_at": "2012-05-17T07:00:00.000Z",
+        "store_name": "Crist - Romaguera",
+        "helpful_score": 140,
+        "name": "Rowan Kling",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/heykenneth/128.jpg"
+    },
+    {
+        "id": 871,
+        "rating": 5,
+        "title": "ut qui officia",
+        "text_body": "Voluptas necessitatibus aliquam magni. Quisquam molestias quam fugiat. Praesentium rerum optio deserunt ea tempora et molestias id.",
+        "created_at": "2013-01-16T08:00:00.000Z",
+        "store_name": "Reichert and Sons",
+        "helpful_score": 383,
+        "name": "Gabriel Bosco",
+        "thumbnail_url": "https://s3.amazonaws.com/uifaces/faces/twitter/SULiik/128.jpg"
+    }
+]
