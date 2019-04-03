bf8fd0777ba0259db3aa890ac7267361aa38b85c
Adrienne Lim <adriennelim.mc@gmail.com>
2019-04-03 12:56:52 -0700

    started working on rendering the bar chart

--- client/src/components/ReviewsSummary.jsx
+++ client/src/components/ReviewsSummary.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import getStars from '../../utils/getStars.js';
 
 const ReviewsSummary = ({ reviews }) => {
+    const ratings = [5,4,3,2,1];
     let averageRating = 0;
     if (reviews.length) {
         averageRating = getAverageRating(reviews)
@@ -16,8 +17,18 @@ const ReviewsSummary = ({ reviews }) => {
                     <span className="star-rating">{getStars(Math.round(averageRating))}</span> 
                     <div><a className="all-reviews" href="#">{`${reviews.length} reviews`}</a></div>
                 </div>
-                <div className="summary-chart">
-                    <div>Summary chart</div>
+                <div className="container-summary-chart">
+                    <div className="ratings-chart">
+                        {ratings.map((rating) => {
+                            return (
+                                <div className="container-bar">
+                                    <span>{rating}  </span>
+                                    <span className="bar">bar</span>
+                                    <span className="bar-percentage">  {getPercentage(reviews,rating)}</span>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
@@ -30,6 +41,11 @@ const getAverageRating = (reviews) => {
         return sum + review.rating;
     }, 0)
     return Number( (sumOfRatings / reviews.length).toFixed(1) );
+};
+
+const getPercentage = (reviews, rating) => {
+    let filtered = reviews.filter((review) => { return review.rating === rating})
+    return Math.round(filtered.length / reviews.length * 100) + '%'
 };
 
 export default ReviewsSummary;
