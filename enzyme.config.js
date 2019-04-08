15f994ae749c57a6d5ae31e0d87f07fc23777f28
Adrienne Lim <adriennelim.mc@gmail.com>
2019-04-08 11:02:03 -0700

    solved issue with transpiling, es6 syntax recognition and css modules

--- enzyme.config.js
+++ enzyme.config.js
@@ -1,11 +1,4 @@
-/*
 import { configure } from 'enzyme'; 
 import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() }); 
-*/
-
-const configure = require('enzyme').configure;
-const Adapter = require('enzyme-adapter-react-16');
 
 configure({ adapter: new Adapter() }); 
